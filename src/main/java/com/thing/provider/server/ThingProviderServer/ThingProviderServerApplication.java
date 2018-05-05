b7a37a5a109be8b406ca89ab18d3475aa56ac3a5
Kevin Martins <kevinmmartins@gmail.com>
2018-05-05 20:23:32 -0300

    fix

--- src/main/java/com/thing/provider/server/ThingProviderServer/ThingProviderServerApplication.java
+++ src/main/java/com/thing/provider/server/ThingProviderServer/ThingProviderServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ThingProviderServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ThingProviderServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ThingProviderServerApplication.class, args);
+    }
 }
